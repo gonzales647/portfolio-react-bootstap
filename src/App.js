a00f8448127f26c0ae8054a44ec7f08b30148a59
gonzales647 <82166951+gonzales647@users.noreply.github.com>
2021-04-16 15:23:00 -0700

    added files to build portfoliio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react'
+import Navbar from "./Navbar"
+import Home from './Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hi, My name is James
-      </header>
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+
+
+const Navbar = () => {
+    return (
+        <div classname='nav'>
+            <nav className="navbar navbar-default">
+                <h1>James's Portfolio</h1>
+                <div className="nav">
+                    <a href='/'>Home</a>
+                    <a href='/'>About</a>
+                    <a href='/create'>Other Page</a>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
+
+export default Navbar;
